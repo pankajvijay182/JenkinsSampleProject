dab4c38a9f99dc8a9760a1cfc3e39e1d60eecbc8
PANVIJAY-T480\PANVIJAY <pankaj.vijay@oracle.com>
2021-06-13 10:45:14 +0530

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package practising.jenkins;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("------------*****Hello World & welcome to Jenkins training*********-------");
+
+	}
+
+}
