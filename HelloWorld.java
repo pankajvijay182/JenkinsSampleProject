111d1566c55e8112a383bad72ad3f5b73dc0d617
pankajvijay182 <85695762+pankajvijay182@users.noreply.github.com>
2021-06-13 12:16:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package practising.jenkins;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("------------*****Hello World & welcome to Jenkins training*********-------");
+		System.out.println("------------*****made some changes*********-------");
 
 	}
 
